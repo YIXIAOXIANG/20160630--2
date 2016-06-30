1fa3f0cab7aace1409a015729f1514fb84dba7f0
mac <mac@macdemacbook-pro-107.local>
2016-06-30 17:35:46 +0800

    good

---
+++ Git-20160630/Git-20160630/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git-20160630
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 毅鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git-20160630/Git-20160630/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git-20160630
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 毅鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
